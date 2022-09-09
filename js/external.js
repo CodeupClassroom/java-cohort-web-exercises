3622a202efe9f888f31b02508f681e62da167040
kennethhowell <howell.kenneth.a@gmail.com>
2022-09-09 16:24:37 -0500

    complete through prompt for favorite color - interaction tested and working successfully

--- js/external.js
+++ js/external.js
@@ -1,3 +1,21 @@
 "use strict";
 
 console.log("Hello from external JavaScript - glad to be here as well :)");
+
+alert("Welcome to my website!");
+
+// Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
+//
+//     For example, if the user enters "blue", your code should alert a message that says:
+//
+//     "Great, blue is my favorite color too!"
+
+//prompt - ask favorite color from user
+//Grab the user's response - for an alert
+
+let userInput = prompt("What is your favorite color, USER");
+
+//alert - looks like I need to use what the user typed in INSIDE of my alert message back OUT to the user [verde] "hey love [verde] too"; [blue] "hey love [blue] too"
+
+alert("Hey " + userInput.trim() + ", I like that color too!")
+
