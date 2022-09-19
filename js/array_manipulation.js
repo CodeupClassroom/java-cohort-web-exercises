3b462227fb1835c6e5840c704cee71f7f0c5fc9a
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-09-19 16:52:29 -0500

    array-manipulation

---
+++ js/array_manipulation.js
@@ -0,0 +1,84 @@
+// Array Manipulation
+
+let animals = ['frog', 'bat', 'camel', 'bumble bee', 'wombat']
+
+// let pixarMovies = ['up', 'a bug\'s life', 'toy story', 'monster\'s inc', 'finding nemo']
+
+let favorites = ['pizza', 18, 'autumn', 2016, ['pepperoni & jalapeños', 'hawaiian', '']]
+
+// adding movies
+
+// methods - functions that are attached to an object
+// forEach()
+
+// push() --> add an element to the end of an array
+// Kiley's favorite animal is dog
+// arrayYouWantToAddTo.push('dog')
+
+// console.log(animals)
+// animals.push('dog');
+// console.log(animals)
+//
+// animals.push('cat', 'giraffe', 'human')
+// console.log(animals)
+
+// let animals = ['frog', 'bat', 'camel', 'bumble bee', 'wombat', [1,2,3,4,5]]
+
+
+// let places = ['mcdonalds', 'texas', 'the ocean', 'my house', ['berlin', 'london', 'prague']]
+
+let zoo = ['zebra', 'lion', 'dolphin', ['cricket', 'bee', 'butterfly'], 'wombat']
+
+// // let symphony = ['strings', 'percussion', 'winds', 'conductor']
+// let symphony = [['violin', 'cello', 'viola'], ['drumset', 'triangle', 'cymbals'], ['flute', 'clarinet', 'saxophone'], 'conductor']
+//
+// //
+// console.log(symphony)
+// // symphony.push('opera singer');
+// // console.log(symphony)
+
+
+
+// unshift
+
+// add 'frog' at the beginning of the 'zoo' array using 'unshift()' method
+
+
+// push
+// -> adds to the end of the array
+// -> returns the new length of the original array
+// unshift
+// -> adds the beginning of the array
+// -> returns the new length of the original array
+
+
+
+
+// removing movies
+
+// .pop()
+// -> remove last el from an arr
+// -> returns the element removed
+
+// array.pop()
+
+let pixarMovies = ['up', 'a bug\'s life', 'toy story', 'monster\'s inc', 'finding nemo']
+
+let removedMovie = pixarMovies.pop()
+
+console.log("removedMovie: ", removedMovie);
+
+// .shift()
+// -> removes the first el from an arr
+// -> returns the element removed
+
+
+
+// accessing movies
+// nameOfTheArray[indexOfTheElYouWantToAccess]
+
+
+
+
+
+// nested arrays
