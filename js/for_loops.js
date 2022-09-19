db226efcbdc46c01ac99dd6248b55ed26de0efda
kennethhowell <howell.kenneth.a@gmail.com>
2022-09-19 11:23:08 -0500

    finished first part of loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,85 @@
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+//
+// For example, showMultiplicationTable(7) should output
+//
+// 7 x 1 = 7
+// 7 x 2 = 14
+// 7 x 3 = 21
+// 7 x 4 = 28
+// 7 x 5 = 35
+// 7 x 6 = 42
+// 7 x 7 = 49
+// 7 x 8 = 56
+// 7 x 9 = 63
+// 7 x 10 = 70
+
+//function showMultiplicationTable
+//"a number" - one parameter
+//console.logs the multiplication table for that number (just multiply by the numbers 1 through 10) [sounds like a FOR LOOP up to desired number]
+
+function showMultiplicationTable(number){
+
+    //First step- can I console.log numbers between 1 - 10? If I can, I can refactor that code to be more complicated ("show multiplication tables")
+    //The second step was to add in our parameter and see it coming through live in the .log
+    //The final step was to add an area for our math in the .log to evaluate and show the result of that math
+    for(let i = 1; i <= 10; i++){
+
+        //Good ol' string concatenation - no problems here, still a perfect way to write things
+        // console.log(number + " x " + i + " = " + (number * i));
+
+
+        //Template literals - string templating, hm. Using 'back ticks' I can start accessing even MORE advanced parts of javascript
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+        //https://www.w3schools.com/js/js_string_templates.asp
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+
+
+}
+
+
+
+
+//
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+//
+//     123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
+//
+// Create a for loop that uses console.log to create the output shown below.
+//
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+//
+// Create a for loop that uses console.log to create the output shown below.
+//
+// 100
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
