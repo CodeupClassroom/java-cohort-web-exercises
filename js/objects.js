cbec9cdeff6a7e3d2f1eb6a3a907fe1e54397714
kennethhowell <howell.kenneth.a@gmail.com>
2022-09-22 11:02:24 -0500

    sayHello method created and tested to satisfaction

--- js/objects.js
+++ js/objects.js
@@ -39,6 +39,16 @@
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
 
+    person.sayHello = function(){
+        //es6 friendly
+        // console.log(`Hello from ${this.firstName} ${this.lastName}!`);
+
+        //string concat friendly
+        console.log("Hello from" + this.firstName + " " + this.lastName);
+    }
+
+    person.sayHello()
+
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
      * more than $200. If a shopper spends more than $200, they get a 12%
